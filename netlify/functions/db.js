3731ddc968a9a0b45af9cb91b367519d6ccc99c0
saulbruh <125154086+saulbruh@users.noreply.github.com>
2025-03-02 13:40:28 -0400

    commit

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 
 exports.handler = async (event, context) => {
     const connection = await mysql.createConnection({
-        host: process.env.mysql.railway.internal,
-        user: process.env.root,
-        password: process.env.jPfAWXnyWXACEHzSKWtjtJpoPDekYyLK,
-        database: process.env.railway,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     });
 
     const [rows] = await connection.execute('SELECT * FROM users');
